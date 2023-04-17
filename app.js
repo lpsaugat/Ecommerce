8f02825357ec17e440b38499a28cd1365ef5dd72
Saugat Paudel <lpsaugat@gmail.com>
2023-04-17 16:15:25 +0545

    2023/04/17

--- app.js
+++ app.js
@@ -1,64 +1,47 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const express = require('express')
- myConnection = require('express-myconnection');
-path = require('path')
-morgan = require('morgan');
-const mongoose = require('mongoose');
-const fs = require('fs');
-const bodyParser = require('body-parser');
+const express = require("express");
+myConnection = require("express-myconnection");
+path = require("path");
+morgan = require("morgan");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
+// const bcrypt = require('bcrypt');
+// const salt = bcrypt.genSaltSync(10);
 
-
-
-
-
-const app = express() 
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(bodyParser.json());
 
+const customer_route = require("./routes/customer_route");
+const { resourceUsage } = require("process");
 
+app.use("/public", express.static("public"));
 
-
-
-
-
-
-
-const customer_route = require('./routes/customer_route');
- const { resourceUsage } = require('process');
-
- 
-
-app.use('/public', express.static('public'));
-
-app.use('/', customer_route);
-
+app.use("/", customer_route);
 
 // app.set('port', process.env.PORT || 3000,);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-
-mongoose 
-.connect(process.env.MONGO_URL)
-.then(()=>console.log("DBconnection Successful") ).catch((err)=>{
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DBconnection Successful"))
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-const ip = require('ip');
+const ip = require("ip");
 
-
-app.listen(3000, ip.address(), () => console.log(`The server is running on port ${process.env.PORT}`))
+app.listen(3000, ip.address(), () =>
+  console.log(`The server is running on port ${process.env.PORT}`)
+);
 
 // mongoose.connection.once('open', () => {
 //     app.listen(app.get('port')
-// , '192.168.101.14' 
+// , '192.168.101.14'
 // );
 // })
-
-
-
-
-
