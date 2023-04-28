d72a5759eced8d021b1e1deadce932cb05853ee1
Saugat Paudel <lpsaugat@gmail.com>
2023-04-28 15:06:06 +0545

    Add: Add to Cart from frontend

--- public/popup.js
+++ public/popup.js
@@ -1,50 +1,28 @@
 var popper = document.getElementById("popup");
 
-function popupf(element){
-     if (popper.classList.contains('hidden') && (screen.width >= 1024)){
-         
-        
-        popper.classList.remove('hidden');
-        popper.style='flex';
-        document.body.style.overflow = 'hidden';
-       
-        
-     
-        
+function popupf(element) {
+  if (popper.classList.contains("hidden") && screen.width >= 1024) {
+    popper.classList.remove("hidden");
+    popper.style = "flex";
+    document.body.style.overflow = "hidden";
+  } else {
+    if (element === 0) {
+      setTimeout(() => {
+        popper.classList.add("hidden");
+        popper.style = "hidden";
+        document.body.style.overflow = "auto";
+      }, 300);
+    } else {
+      popper.classList.add("hidden");
+      popper.style = "hidden";
+      document.body.style.overflow = "auto";
     }
-   
-    else{
-        if (element === 0){
-            setTimeout(() => {
-                popper.classList.add('hidden');
-                popper.style='hidden';
-                document.body.style.overflow = 'auto';              }, 300);
-        }
-        else {
-            popper.classList.add('hidden');
-                popper.style='hidden';
-                document.body.style.overflow = 'auto';
-        }
-        
-    }
-
-    
-    
-     
-   
-
+  }
 }
 
 // var counter = document.querySelectorAll('counterforadd');
 
 // var addtocart = document.getElementsByClassName('added');
-
-
-function addedtocart(element){
-        hideandshow(element.querySelector('.added'));
-
-     
-}
 
 // function cardshadow(element){
 //     var card = element.querySelector.('payment');
