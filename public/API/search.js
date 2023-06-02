59a93e18def3f19ebe193f74effb451e166dd7fa
Saugat Paudel <lpsaugat@gmail.com>
2023-06-02 13:11:15 +0545

    Add: Fetch search data

---
+++ public/API/search.js
@@ -0,0 +1,29 @@
+const search = document.querySelector("#search");
+
+if (search) {
+  search.addEventListener("submit", (event) => {
+    console.log("jkjkj");
+
+    event.preventDefault();
+
+    const formData = new FormData(search);
+    const name = formData.get("query");
+
+    fetch(`http://192.168.1.73:3000/search`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+}
