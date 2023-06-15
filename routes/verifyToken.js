c032bce7901c9c5619550f03eb0702b6fa3ab162
Saugat Paudel <lpsaugat@gmail.com>
2023-06-15 13:22:35 +0545

    Add: Made to render webpage with either  Token or not

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,5 +1,33 @@
 const jwt = require("jsonwebtoken");
 
+const verifyTokenOrNon = (req, res, next) => {
+  if (req.cookies["Token"]) {
+    const authHeader = req.cookies["Token"];
+    if (authHeader) {
+      const token = authHeader;
+      jwt.verify(token.value, process.env.JWT_SECRET, (err, user) => {
+        if (err) res.status(403).json("Token is not validd!");
+        req.user = user;
+        next();
+      });
+    } else {
+      return res.status(401).json("You are not authenticated");
+    }
+  } else {
+    const authHeader = req.headers.authorization;
+    if (authHeader && authHeader.startsWith("Bearer")) {
+      const token = authHeader.split(" ")[1];
+      jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) res.status(403).json("Token is not valid!");
+        req.user = user;
+        next();
+      });
+    } else {
+      next();
+    }
+  }
+};
+
 const verifyToken = (req, res, next) => {
   if (req.cookies["Token"]) {
     const authHeader = req.cookies["Token"];
@@ -54,6 +82,8 @@ const verifyTokenAndAdmin = (req, res, next) => {
 };
 
 module.exports = {
+  verifyTokenOrNon,
+
   verifyToken,
   verifyTokenAndAuthorization,
   verifyTokenAndAdmin,
