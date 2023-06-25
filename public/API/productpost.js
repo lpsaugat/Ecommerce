6051b141c3af6f7acb410d5aa9e88b0250fea37c
Saugat Paudel <lpsaugat@gmail.com>
2023-06-25 20:46:55 +0545

    Add: Add to cart a single product with quantity

--- public/API/productpost.js
+++ public/API/productpost.js
@@ -55,3 +55,29 @@ function deleteProduct(productID) {
       console.log(error);
     });
 }
+
+function addedtocartSingle(element, productID, price) {
+  console.log(productID, "id");
+  fetch(`http://${ipAddress}:3000/order`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      productID,
+      quantity: quantitybox[0].value,
+      price,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Do something with the response data
+      element.querySelector(".added").style.display = "block";
+    })
+
+    .catch((error) => {
+      console.log();
+      console.error(error);
+    });
+}
