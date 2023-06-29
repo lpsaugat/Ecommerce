db7023fd236908e6b27cb335f08da00be83ee347
Saugat Paudel <lpsaugat@gmail.com>
2023-06-29 12:06:47 +0545

    Add: PopUp Image and View Detail

--- public/API/popUpFetch.js
+++ public/API/popUpFetch.js
@@ -13,11 +13,19 @@ function popUpFetch(element) {
       console.log(data.popUp.name);
       const productName = data.popUp.name;
       const productDesc = data.popUp.description;
+      const productImage = data.popUp.image;
+      const productID = data.popUp._id;
+
       const productPrice = data.popUp.price.$numberDecimal;
       console.log(productPrice);
       var popUpName = document.getElementById("popUpName");
       var popUpPrice = document.getElementById("popUpPrice");
       var popUpDescription = document.getElementById("popUpDescription");
+      var popUpImage = document.getElementById("popUpImage");
+      var popUpViewDetail = document.getElementById("popUpViewDetail");
+
+      popUpImage.src = productImage;
+      popUpViewDetail.href = `/product/${productID}`;
 
       popUpName.textContent = productName;
       popUpDescription.textContent = productDesc;
