937888f792879353733bb91bce1b6428c7af5f45
Saugat Paudel <lpsaugat@gmail.com>
2023-06-30 15:19:45 +0545

    Add: Reviews Timestamps

--- models/Review.js
+++ models/Review.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
-const Review = new mongoose.Schema({
-  comment: {
-    type: String,
+const Review = new mongoose.Schema(
+  {
+    comment: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    productID: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+    },
+    packageID: {
+      type: mongoose.Types.ObjectId,
+      ref: "Package",
+    },
   },
-  rating: {
-    type: Number,
-  },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
-  productID: {
-    type: mongoose.Types.ObjectId,
-    ref: "Product",
-  },
-  packageID: {
-    type: mongoose.Types.ObjectId,
-    ref: "Package",
-  },
-});
+  { timestamps: true }
+);
 module.exports = mongoose.model("Review", Review);
