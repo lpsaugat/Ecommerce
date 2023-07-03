20312efe0a1c82198ec3b1ab9a18c8a9e21adcff
Saugat Paudel <lpsaugat@gmail.com>
2023-07-03 10:39:58 +0545

    Add: Package Review and Ratings

--- public/API/addReview.js
+++ public/API/addReview.js
@@ -27,3 +27,33 @@ if (productReview) {
       });
   });
 }
+
+const packageReview = document.getElementById("packageReview");
+
+if (packageReview) {
+  packageReview.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(packageReview);
+    const comment = formData.get("comment");
+    const rating = formData.get("rating");
+    console.log(rating);
+    const packageID = formData.get("packageID");
+    fetch(`http://${ipAddress}:3000/reviewPackage/${packageID}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        comment,
+        rating,
+      }),
+    })
+      .then((data) => {
+        // Do something with the response data
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+}
