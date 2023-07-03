36d9bb33c7e115f6fd6ff4da60d8e65fb92de75f
Saugat Paudel <lpsaugat@gmail.com>
2023-07-03 07:37:53 +0545

    Update: Set and send rating from frontend

--- public/JS/rating.js
+++ public/JS/rating.js
@@ -1,47 +1,25 @@
 var ratingbutton = document.querySelectorAll(".ratingbutton");
 var rate = document.querySelectorAll(".star");
 var unrate = document.querySelectorAll(".star-shade");
-
+var ratingID = document.getElementById("ratingID");
 function rating(index, element) {
-//   for (i = 0; i < rate.length; i++) {
-//     rate[i].style.display = "none";
+  const rates = element.parentNode.querySelectorAll(".ratingbutton");
 
-//     unrate[i].style.display = "block";
-//   }
+  for (let i = 0; i < rates.length; i++) {
+    const r = rates[i];
+    r.querySelector(".star").style.display = "none";
+    r.querySelector(".star-shade").style.display = "block";
+  }
 
-//   for (i = 0; i < rate.length; i++) {
-//     rate[i].style.display = "block";
-//     unrate[i].style.display = "none";
-//     if (i === element) {
-//       i = rate.length;
-//     }
+  for (let i = 0; i < rates.length; i++) {
+    const r = rates[i];
 
-//     console.log(i)
-//   }
+    r.querySelector(".star-shade").style.display = "none";
+    r.querySelector(".star").style.display = "block";
 
-
-    const rates = element.parentNode.querySelectorAll(".ratingbutton");
-
-    for(let i=0; i<rates.length; i++){
-        const r = rates[i];
-        r.querySelector(".star").style.display = "none"
-        r.querySelector(".star-shade").style.display = "block"
+    if (index === i) {
+      i = rates.length;
+      ratingID.value = i;
     }
-
-    for(let i=0; i<rates.length; i++){
-        const r = rates[i];
-
-        r.querySelector(".star-shade").style.display = "none"
-        r.querySelector(".star").style.display = "block"
-
-
-        
-        if(index === i){
-            i = rates.length;
-        }
-
-
-    }
-
-
+  }
 }
