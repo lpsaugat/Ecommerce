ff70850bac1f7624e4bc3832cc5695cc782d756b
Saugat Paudel <lpsaugat@gmail.com>
2023-08-02 08:56:07 +0545

    Update: Category timestamps

--- models/Categories.js
+++ models/Categories.js
@@ -1,30 +1,33 @@
 const mongoose = require("mongoose");
 
 const slugify = require("slugify");
-const Category = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
+const Category = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: [String],
+    },
+    slug: {
+      type: String,
+    },
+    parentCategory: {
+      type: [String],
+      ref: "Category",
+    },
+    totalProducts: {
+      type: Number,
+    },
+    status: {
+      type: Boolean,
+    },
   },
-  description: {
-    type: String,
-  },
-  image: {
-    type: [String],
-  },
-  slug: {
-    type: String,
-  },
-  parentCategory: {
-    type: [String],
-    ref: "Category",
-  },
-  totalProducts: {
-    type: Number,
-  },
-  status: {
-    type: Boolean,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Category", Category);
