3e3ada08bd897e7ce23eaf29571af575cfe7a93d
Saugat Paudel <lpsaugat@gmail.com>
2023-08-16 07:51:25 +0545

    Modify: theme of alerts, timeout

--- public/API/adminAPI/alert.js
+++ public/API/adminAPI/alert.js
@@ -6,11 +6,24 @@ function showAlert(title, message) {
 
   alertTitle.textContent = title;
   alertMessage.textContent = message;
+  alertModal.style.opacity = "1"; // Set initial opacity to 1
   alertModal.style.display = "block";
 
   okButton.addEventListener("click", function () {
-    alertModal.style.display = "none";
+    hideModal(alertModal);
   });
+
+  setTimeout(function () {
+    hideModal(alertModal); // Start the fade-out effect after a certain timeout
+  }, 2000);
+}
+
+function hideModal(modal) {
+  modal.style.opacity = "0"; // Set opacity to 0 to initiate fade-out
+  setTimeout(function () {
+    modal.style.display = "none";
+    modal.style.opacity = "1"; // Reset opacity for next use
+  }, 500); // Wait for the fade-out animation to complete before hiding the modal
 }
 var close = document.getElementsByClassName("closebtn");
 var i;
@@ -18,8 +31,7 @@ var i;
 // Loop through all close buttons
 for (i = 0; i < close.length; i++) {
   // When someone clicks on a close button
-  close[i].onclick = function(){
-
+  close[i].onclick = function () {
     // Get the parent of <span class="closebtn"> (<div class="alert">)
     var div = this.parentElement;
 
@@ -27,6 +39,8 @@ for (i = 0; i < close.length; i++) {
     div.style.opacity = "0";
 
     // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
-    setTimeout(function(){ div.style.display = "none"; }, 600);
-  }
+    setTimeout(function () {
+      div.style.display = "none";
+    }, 2000);
+  };
 }
