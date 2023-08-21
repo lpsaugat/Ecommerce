b38a873a1a75273bcac1d1a1b588c49f6328d4b1
Saugat Paudel <lpsaugat@gmail.com>
2023-08-21 09:46:33 +0545

    Add: Update Delivery

--- public/API/adminAPI/delivery.js
+++ public/API/adminAPI/delivery.js
@@ -1,4 +1,5 @@
 const addDelivery = document.querySelector("#addDelivery");
+const updateDelivery = document.querySelector("#updateDelivery");
 
 if (addDelivery) {
   addDelivery.addEventListener("submit", (event) => {
@@ -36,6 +37,51 @@ if (addDelivery) {
           );
         } else {
           showAlert("Something Went Wrong", "Delivery wasn't added", true);
+        }
+      })
+      .catch((error) => {
+        showAlert("Error");
+        console.error(error);
+      });
+  });
+}
+
+if (updateDelivery) {
+  updateDelivery.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(updateDelivery);
+    const name = formData.get("name");
+    const price = formData.get("price");
+    const location = formData.get("location");
+    const description = formData.get("description");
+    const requestBody = new FormData();
+
+    requestBody.append("name", name);
+    requestBody.append("price", price);
+    requestBody.append("location", location);
+
+    requestBody.append("description", description);
+
+    const formDataArray = Array.from(requestBody.entries());
+
+    var sendRequest = formDataArray.filter(([name, value]) => value !== "");
+    const convertedFormData = new FormData();
+    sendRequest.forEach(([name, value]) => {
+      convertedFormData.append(name, value);
+    });
+    fetch(`http://${ipAddress}:3000/admindashboard/delivery/:id`, {
+      method: "PUT",
+      headers: {},
+      body: convertedFormData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          showAlert(
+            "Delivery Updated",
+            "The Delivery has been updated successfully."
+          );
+        } else {
+          showAlert("Something Went Wrong", "Delivery wasn't updated", true);
         }
       })
       .catch((error) => {
