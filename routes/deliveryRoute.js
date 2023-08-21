6ed669a58f7976ab2d84bc340b5f80ee292281e9
Saugat Paudel <lpsaugat@gmail.com>
2023-08-21 08:58:23 +0545

    Update: Delivery view all route

--- routes/deliveryRoute.js
+++ routes/deliveryRoute.js
@@ -6,37 +6,44 @@ const {
   verifyTokenAndAdmin,
 } = require("./verifyToken");
 
+//Delivery View all Page
+router.get(
+  "/admindashboard/delivery",
+  verifyTokenAndAdmin,
+  deliveryController.deliveryView
+);
+
 //Delivery Add
 router.post(
   "/admindashboard/delivery",
   verifyTokenAndAdmin,
-  productController.deliveryAdd
+  deliveryController.deliveryAdd
 );
 
 //Delivery Add Page
 router.get(
   "/admindashboard/deliveryAdd",
   verifyTokenAndAdmin,
-  productController.deliveryAddPage
+  deliveryController.deliveryAddPage
 );
 
 //Delivery Edit
 router.patch(
   "/admindashboard/delivery/:id",
   verifyTokenAndAdmin,
-  productController.deliveryEdit
+  deliveryController.deliveryEdit
 );
 
 //Delivery Edit/Single Page
 router.get(
   "/admindashboard/deliveryEdit/:id",
   verifyTokenAndAdmin,
-  productController.deliveryEditPage
+  deliveryController.deliveryEditPage
 );
 
 //Delivery Delete
 router.delete(
   "/admindashboard/delivery/:id",
   verifyTokenAndAdmin,
-  productController.deliveryDelete
+  deliveryController.deliveryDelete
 );
